2e16481a77ee3815ab6bcecca34e205115b885d6
sonukumar9939 <sonukumar.75421@gmail.com>
2020-04-18 23:23:34 +0530

    Discovery Service

---
+++ src/main/java/com/api/gateway/EurekaServiceApplication/EurekaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.api.gateway.EurekaServiceApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceApplication.class, args);
+	}
+
+}
